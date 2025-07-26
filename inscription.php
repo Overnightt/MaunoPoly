0e32be15ee294eae2ed69db5738169f3d4264d62
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-07-26 17:48:28 +0200

    Add files via upload

---
+++ inscription.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+$conn = new mysqli ("sql112.infinityfree.com","if0_39565548","23ubIau4FKcAK", "if0_39565548_maunoply");
+$username = $_POST['username'];
+$password = $_POST['pwd'];
+$sql = "INSERT INTO maunopoly (username, password) VALUES (?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $username, $password);  
+$stmt->execute();
+$stmt->close(); 
+$conn->close();
+?>
