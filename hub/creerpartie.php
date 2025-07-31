254930ada2da66aa6296e950800d628820d3b90e
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-07-31 15:01:37 +0200

    Update creerpartie.php

--- hub/creerpartie.php
+++ hub/creerpartie.php
@@ -11,9 +11,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $joueurs = 1;
     $stmt->bind_param("ssi", $host, $statut, $joueurs);
     $stmt->execute();
+    $id_partie = $conn->insert_id;
     $stmt->close();
+    $res = $conn->query("SELECT player_id FROM joueurs WHERE username = '$host'");
+    $row = $res->fetch_assoc();
+    $id_joueur = $row['player_id'];
+    $conn->query("INSERT INTO joueur_partie (id_joueur, id_partie) VALUES ($id_joueur, $id_partie)");
 }
 $conn->close();
-header("Location: hub.html");
+header("Location: saloon.html");
 exit;
 ?>
