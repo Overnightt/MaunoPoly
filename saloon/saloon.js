67e461cb690d1bbe125c6bccc6bb434ea0165117
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-08-14 16:26:24 +0200

    Update saloon.js

--- saloon/saloon.js
+++ saloon/saloon.js
@@ -1,5 +1,15 @@
+function updateListejoueur() {
 fetch("chercherjouer.php")
     .then(response => response.json())
         .then(data => {
-
+            const playerListDiv = document.getElementById("liste-joueurs");
+            playerListDiv.innerHTML = "";
+            data.forEach(player => {
+                const p = document.createElement("p");
+                p.textContent = player.username; 
+                playerListDiv.appendChild(p);
+            });
         });
+}
+updateListejoueur();
+setInterval(updateListejoueur, 2000);
