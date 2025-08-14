b703d45d1e69788c73e36a856fc1d375d5d8ac08
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-08-14 16:26:02 +0200

    Update chercherjoueur.php

--- saloon/chercherjoueur.php
+++ saloon/chercherjoueur.php
@@ -1,10 +1,12 @@
 <?php
+session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $conn = new mysqli("sql112.infinityfree.com", "if0_39565548", "23ubIau4FKcAK", "if0_39565548_maunoply");
 $sql = "SELECT joueurs.username FROM joueur_partie JOIN joueurs ON joueur_partie.id_joueur = joueurs.player_id WHERE joueur_partie.id_partie = ?";
 $stmt = $conn->prepare($sql);
+$id_partie = $_SESSION['id_partie'];
 $stmt->bind_param("i", $id_partie);
 $stmt->execute();
 $result = $stmt->get_result();
@@ -13,7 +15,8 @@ while ($row = $result->fetch_assoc()) {
     $joueur[] = $row['username'];
 }
 header('Content-Type: application/json');
-echo json_encode($players);
+echo json_encode($joueur);
 $stmt->close();
 $conn->close();
 ?>
+
