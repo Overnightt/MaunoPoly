5fe5c041bb6f63b3e1fad3b37b8d4325ce357c98
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-08-17 11:50:43 +0200

    Update commencerpartie.php

--- saloon/commencerpartie.php
+++ saloon/commencerpartie.php
@@ -4,10 +4,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $conn = new mysqli("sql112.infinityfree.com", "if0_39565548", "23ubIau4FKcAK", "if0_39565548_maunoply");
-$sql = "UPDATE parties SET status = 'started' WHERE host = ?";
+$sql = "UPDATE parties SET statut = 'en-cours' WHERE host = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $_SESSION['user']);
 $stmt->execute();
+echo "ok";
 $stmt->close();
 $conn->close()
 ?>
