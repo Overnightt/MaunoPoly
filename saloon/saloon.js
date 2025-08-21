24f0c773f88e7764d3d2de4b2c1743882a3a72aa
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-08-21 17:14:09 +0200

    Update saloon.js

--- saloon/saloon.js
+++ saloon/saloon.js
@@ -1,28 +1,27 @@
-function updateListejoueur() {
-fetch("chercherjoueur.php")
-    .then(response => response.json())
-        .then(data => {
-            const playerListDiv = document.getElementById("liste-joueurs");
-            playerListDiv.innerHTML = "";
-            data.forEach(username => {
-                const li = document.createElement("li");
-                li.textContent = username; 
-                playerListDiv.appendChild(li);
-            });
-        });
-}
-
-function checkPartie() {
-    fetch("checkstatut.php")
-        .then(response => response.text())
-        .then(statut => {
-            if (statut === "en-cours") {
-                window.location.href = "game.html"; 
-            }
-        });
-}
-
 document.addEventListener("DOMContentLoaded", () => {
+    function updateListejoueur() {
+    fetch("chercherjoueur.php")
+        .then(response => response.json())
+            .then(data => {
+                const playerListDiv = document.getElementById("liste-joueurs");
+                playerListDiv.innerHTML = "";
+                data.forEach(username => {
+                    const li = document.createElement("li");
+                    li.textContent = username; 
+                    playerListDiv.appendChild(li);
+                });
+            });
+    }
+
+    function checkPartie() {
+        fetch("checkstatut.php")
+            .then(response => response.text())
+            .then(statut => {
+                if (statut === "en-cours") {
+                    window.location.href = "game.html"; 
+                }
+            });
+    }
     const startBtn = document.getElementById("start-btn");
     if (startBtn) { 
         startBtn.addEventListener("click", () => {
@@ -35,8 +34,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 })
         });
     }
+    updateListejoueur();
+    checkPartie();
+    setInterval(checkPartie, 2000);
+    setInterval(updateListejoueur, 2000);
 });
-updateListejoueur();
-checkPartie();
-setInterval(checkPartie, 2000);
-setInterval(updateListejoueur, 2000);
+
