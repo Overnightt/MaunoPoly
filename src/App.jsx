15d12bd19b9528c089434c640cc85a75913473db
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-08-28 15:35:55 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const Game = () => {
+    return (
+        <>
+            <div>
+                <img src="https://www.monopolypedia.fr/editions/france/classique2014/monopoly-classique-plateau.jpg"/>
+            </div>
+        </>
+    );
+}
+export default Game;
