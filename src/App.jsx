679e76f2d21a1748723206ac5649825967698d01
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-08-29 10:49:46 +0200

    Change image source in Game component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ const Game = () => {
     return (
         <>
             <div>
-                <img src="https://www.monopolypedia.fr/editions/france/classique2014/monopoly-classique-plateau.jpg"/>
+                <img src="plateau.jpg"/>
             </div>
         </>
     );
 }
+
 export default Game;
