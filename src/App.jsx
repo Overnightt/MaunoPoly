cdd6c3cf1fd8ca3776347a93fbfecba96122096f
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-08-29 13:53:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ const Game = () => {
     return (
         <>
             <div >
+                <audio autoPlay loop>
+                    <source src="/OST_Maunopoly.mp3" type="audio/mpeg" />
+                </audio>
                 <img className="plateau" src="plateau.jpg"/>
             </div>
         </>
     );
 }
 export default Game;
+
 
