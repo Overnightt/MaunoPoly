e447f3dcff25ebf2431f840ab4ed247b39edcf74
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-09-04 15:12:20 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import "./App.css"
 import { useState } from 'react';
 
 const Game = () => {
-    const properties = [
-        { id: 1, name: "Boulevard de Belleville", style: { top: "X%", left: "X%", width: "X%", height: "X%" } },
+    const propriétés = [
+        { id: 1, name: "Boulevard de Belleville", style: { top: "86%", left: "79%", width: "8.5%", height: "13%" } },
+        { id: 2, name: "Caissecom1", style: { top: "20%", left: "20%", width: "10%", height: "10%" } },
       ];
     return (
-        <>
-            <div >
+            <div className="Plat0">
                 <audio autoPlay loop>
                     <source src="/OST_Maunopoly.mp3" type="audio/mpeg" />
                 </audio>
                 <img className="plateau" src="plateau.jpg"/>
+                {propriétés.map((p) => (
+                    <div key={p.id} className="zone-propriétés" style={p.style} onClick={()=> alert(`You clicked on ${p.name}`)} />
+                ))}
             </div>
-        </>
     );
 }
 export default Game;
+
 
 
 
